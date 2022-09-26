0908e1872d2aeb068384acf380c86084fe503dee
ivan <ivanni95@gmail.com>
2022-09-26 19:45:55 +0400

    Решил задачу 1

---
+++ src/Main.java
@@ -0,0 +1,11 @@
+public class Main {
+    public static void main(String[] args) {
+      int age = 17;
+      if (age >= 18) {
+          System.out.println("Поздравляем с совершеннолетием!");
+      }
+      if (age < 18){
+          System.out.println("Пока ты не достиг совершеннолетия. Подожди чуть-чуть");
+      }
+    }
+}
