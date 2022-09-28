4714556d29b377f365d4401281e4521dd1af76bf
ivan <ivanni95@gmail.com>
2022-09-28 21:25:14 +0400

    Решил задачу 5

--- src/Main.java
+++ src/Main.java
@@ -5,50 +5,72 @@ public class Main {
       if (age >= 18) {
           System.out.println("Поздравляем с совершеннолетием!");
       }
-      if (age < 18){
+      if (age < 18) {
           System.out.println("Пока ты не достиг совершеннолетия. Подожди чуть-чуть");
 
           // Задача 2
 
-       int age1 = 25;
+          int age1 = 25;
 
-       if (age1 >= 18)
-       if (age1 < 24){
-           System.out.println("Ты закончил школу и можешь пойти учиться дальше в университет");
-       }
-       if (age1 >= 7)
-       if (age1 < 18){
-           System.out.println("Ты учишься в школе");
-       }
-       if (age1 >= 24)
-       {
-           System.out.println("Ты закончил университет. Неплохо было бы найти работу");
+          if (age1 >= 18)
+              if (age1 < 24) {
+                  System.out.println("Ты закончил школу и можешь пойти учиться дальше в университет");
+              }
+          if (age1 >= 7)
+              if (age1 < 18) {
+                  System.out.println("Ты учишься в школе");
+              }
 
-           // Задача 3
+          if (age1 >= 24) {
+              System.out.println("Ты закончил университет. Неплохо было бы найти работу");
 
-           int passengers = 77;
+              // Задача 3
 
-           if (passengers < 60) {
-               System.out.println("В вагоне есть сидячие и стоячие места");
-           }
-           if (passengers >=60)
-           if (passengers < 120) {
-               System.out.println("В вагоне есть только стоячие места");
-           }
-           if (passengers >= 120) {
-               System.out.println("Мест нет");
+              int passengers = 77;
 
-               }
-           //Задача 4
-           int age2 = 19;
-           if (age2 >= 18){
-               System.out.println("Поздравляем с совершеннолетием!");
-           }else{
-               System.out.println("Пока ты не достиг совершеннолетия. Подожди чуть-чуть");
-           }
+              if (passengers < 60) {
+                  System.out.println("В вагоне есть сидячие и стоячие места");
+              }
+              if (passengers >= 60)
+                  if (passengers < 120) {
+                      System.out.println("В вагоне есть только стоячие места");
+                  }
+              if (passengers >= 120) {
+                  System.out.println("Мест нет");
 
-           }
+              }
+              //Задача 4
+              int age2 = 19;
+              if (age2 >= 18) {
+                  System.out.println("Поздравляем с совершеннолетием!");
+              } else {
+                  System.out.println("Пока ты не достиг совершеннолетия. Подожди чуть-чуть");
+              }
+              // Задача 5
+
+              int age5 = 19;
+
+              if (age5 >=24) {
+                  System.out.println("Ты закончил университет. Неплохо было бы найти работу");
+              }else{
+                  if (age5 >=18) {
+                      System.out.println("Ты закончил школу и можешь пойти учиться дальше в университет");
+                  }else{
+                      if (age5 > 7) {
+                          System.out.println("Ты учишься в школе");
+                      }
+
+
+                  }
+              }
+
+
+
+
+          }
+
+      }
        }
       }
-    }
+
 
